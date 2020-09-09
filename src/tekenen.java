301281adadc747b5566703d061baf609d7738395
trist <tvanCaspel@roc-dev.com>
2020-09-09 09:42:47 +0200

    Dit is mijn tekening van een Driehoek

---
+++ src/tekenen.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+public class tekenen extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.drawLine(50, 120, 100, 60);
+        g.drawLine(100, 60, 150, 120);
+        g.drawLine(150, 120, 50, 120);
+
+    }
+}
